effcf697ba16c84186bce2ac529c6fb6fc03b3e8
Zach Marcin <marcinx27@gmail.com>
2017-06-17 22:45:49 -0400

    Added the 'subtract' function.

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -9,4 +9,14 @@ public class Operations {
     public static int add(int x, int y) {
         return x + y;
     }
+
+    /**
+     * The subtraction function
+     * @param x The first operand
+     * @param y The second operand
+     * @return y taken from x
+     */
+    public static int subtract(int x, int y) {
+        return x - y;
+    }
 }
--- src/test/java/OperationsTest.java
+++ src/test/java/OperationsTest.java
@@ -6,4 +6,9 @@ public class OperationsTest {
     public void testAdd() throws Exception {
         Assert.assertEquals(2, Operations.add(1, 1));
     }
+
+    @Test
+    public void testSubtract() throws Exception {
+        Assert.assertEquals(0, Operations.subtract(1, 1));
+    }
 }
